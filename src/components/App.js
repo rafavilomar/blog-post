2a6d48e0919766c7e0990e87946e4cb2ad45f22b
Rafael Vilomar <rafavilomar@gmail.com>
2021-06-24 23:02:32 -0400

    Header updated and home view added

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Post from "../containers/Post";
+import Header from "./layouts/Header";
+
+//VIEWS
+import Home from "../containers/Home"
+import Post from "../containers/posts/Post";
 import Tasks from "../containers/Tasks";
 import Users from "../containers/Users";
-import Header from "./layouts/Header";
+import AllPosts from "../containers/posts/AllPosts";
+
+
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <Switch>
-        <Route exact path="/" component={Users} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/users" component={Users} />
         <Route exact path="/tasks" component={Tasks} />
-        <Route exact path="/post/:key" component={Post} />
+        <Route exact path="/posts/" component={AllPosts} />
+        <Route exact path="/posts/:key" component={Post} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,8 +4,9 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <nav id="header">
-      <Link to="/">Users</Link>
-      <Link to="/tasks">Tasks</Link>
+      <Link to='/'>Home</Link>
+      <Link to="/users">Users</Link>
+      <Link to="/posts">Posts</Link>
     </nav>
   );
 };
