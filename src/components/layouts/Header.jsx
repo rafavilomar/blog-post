9478fd670a49a18b5f32af57b31da35ac4522145
Rafael Vilomar <rafavilomar@gmail.com>
2021-06-27 15:36:12 -0400

    #9 Menu option added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import '../../assets/styles/layout/header.scss'
+import "../../assets/styles/layout/header.scss";
+
+import IconButton from "../buttons/IconButton";
+
+//ICONS
+import { ReactComponent as DropdownIcon } from "../../assets/icons/dropdown.svg";
+import { ReactComponent as PostIcon } from "../../assets/icons/post.svg";
+import { ReactComponent as UsersIcon } from "../../assets/icons/users.svg";
+import { ReactComponent as AddIcon } from "../../assets/icons/add.svg";
 
 const Header = () => {
   return (
-    <div id='header'>
-      <h1 id='brand' >BlogPost</h1>
-      <div style={{width: 200, backgroundColor: 'red', height: 50}} ></div>
-    <nav>
-      <Link to="/users">Users</Link>
-      <Link to="/posts">Posts</Link>
-    </nav>
+    <div id="header">
+      <h1 id="brand">BlogPost</h1>
+      <div id='searchBar'></div>
+      <nav>
+        <Link to="/">
+          <IconButton icon={<PostIcon className="icon-active" height={22} width={22} />} />
+        </Link>
+        <Link to="/users">
+          <IconButton icon={<UsersIcon className="icon" height={22} width={22} />} />
+        </Link>
+        <IconButton icon={<AddIcon className="icon" height={22} width={22} />} />
+        <div id='profile' >
+          <span></span>
+            <DropdownIcon className='icon'/>
+        </div>
+      </nav>
     </div>
   );
 };
