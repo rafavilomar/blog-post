028cc36941321cf505d0efdef68757535dd598c7
Rafael Vilomar <rafavilomar@gmail.com>
2021-06-27 12:17:59 -0400

    #9 Brand name added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import '../../assets/styles/layout/header.scss'
 
 const Header = () => {
   return (
-    <nav id="header">
-      <Link to='/'>Home</Link>
+    <div id='header'>
+      <h1 id='brand' >BlogPost</h1>
+      <div style={{width: 200, backgroundColor: 'red', height: 50}} ></div>
+    <nav>
       <Link to="/users">Users</Link>
       <Link to="/posts">Posts</Link>
     </nav>
+    </div>
   );
 };
 export default Header;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {Provider} from 'react-redux'
 import reducers from './reducers/index'
 
 import './index.css';
+import './assets/styles/main.scss'
 import './assets/styles/icons/eyes.css'
 
 const store = createStore(
