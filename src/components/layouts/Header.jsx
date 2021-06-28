1997a32970c51ae0b9581e97bbf6d448be42cda8
Rafael Vilomar <rafavilomar@gmail.com>
2021-06-27 20:01:28 -0400

    minor fix

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "../../assets/styles/layout/header.scss";
 
 import IconButton from "../buttons/IconButton";
-import Avatar from 'react-nice-avatar'
+import Avatar from "react-nice-avatar";
 
 //ICONS
 import { ReactComponent as DropdownIcon } from "../../assets/icons/dropdown.svg";
@@ -15,22 +15,30 @@ import { ReactComponent as SearchIcon } from "../../assets/icons/search.svg";
 const Header = () => {
   return (
     <div id="header">
-      <h1 id="brand">BlogPost</h1>
-      <div id='searchBar'>
-        <SearchIcon className='icon'/>
-        <input type="text" />
-      </div>
+      <Link to='/' >
+        <h1 id="brand">BlogPost</h1>
+      </Link>
       <nav>
+        <div id="searchBar">
+          <SearchIcon className="icon" />
+          <input type="text" />
+        </div>
         <Link to="/">
-          <IconButton icon={<PostIcon className="icon-active" height={22} width={22} />} />
+          <IconButton
+            icon={<PostIcon className="icon-active" height={22} width={22} />}
+          />
         </Link>
         <Link to="/users">
-          <IconButton icon={<UsersIcon className="icon" height={22} width={22} />} />
+          <IconButton
+            icon={<UsersIcon className="icon" height={22} width={22} />}
+          />
         </Link>
-        <IconButton icon={<AddIcon className="icon" height={22} width={22} />} />
-        <div id='profile' >
-          <Avatar style={{ width: 42, height: 42 }}/>
-            <DropdownIcon className='icon' height={18} width={18}/>
+        <IconButton
+          icon={<AddIcon className="icon" height={22} width={22} />}
+        />
+        <div id="profile">
+          <Avatar style={{ width: 42, height: 42 }} />
+          <DropdownIcon className="icon" height={18} width={18} />
         </div>
       </nav>
     </div>
