090a8ae495cd635088a7ea6568a0c86819b2b25a
Rafael Vilomar <rafavilomar@gmail.com>
2021-06-29 22:22:49 -0400

    close #5. Post published added to each user card

--- src/components/users/UserCard.jsx
+++ src/components/users/UserCard.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../assets/styles/user.scss";
 
 import Avatar from "react-nice-avatar";
 import { Link } from "react-router-dom";
+import { getPost_API } from "../../fetch";
 
 const UserCard = ({ user }) => {
+  const [totalPost, setTotalPost] = useState();
+
+  const getTotalPosts = async () => {
+    let response = await getPost_API(user.id);
+    setTotalPost(response.data.length);
+  };
+
+  useEffect(async () => await getTotalPosts(), []);
+
   return (
     <Link to="/">
       <article className="userCard">
         <Avatar style={{ width: 150, height: 150 }} />
         <div className="userCard__content">
-          <p className="userCard__userName">{"@" + user.username}</p>
+          <p className="userCard__userName">{`@${user.username}`}</p>
           <h4 className="userCard__fullName">{user.name}</h4>
-          <p className="userCard__posts"># Posts</p>
+          <p className="userCard__posts">{`${totalPost} Posts`}</p>
         </div>
       </article>
     </Link>
