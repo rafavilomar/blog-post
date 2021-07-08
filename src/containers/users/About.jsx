7689c69b655c8ef2f6d8819d5d5af78fb2d5bc81
Rafael Vilomar <rafavilomar@gmail.com>
2021-07-07 20:31:27 -0400

    #11 UserSection component added

--- src/containers/users/About.jsx
+++ src/containers/users/About.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from "react";
+import '../../assets/styles/containers/users.scss'
 
 //COMPONENTS
 import Header from "../../components/layouts/Header";
 import Tab from "../../components/users/Tab";
+import UserCard from "../../components/users/UserCard";
+import { getUsersById_API } from "../../fetch";
 
 const About = ({ match }) => {
-  useEffect(() => console.log(match));
+  const [user, setUser] = useState();
+
+  const handleUser = async (id) => {
+    let response = await getUsersById_API(match.params.user);
+    setUser(response.data[0]);
+  };
+
+  useEffect(() => handleUser(), []);
   return (
     <>
       <Header />
-      <div className="container">
+      <div className="container userDetails">
+        {user && <UserCard user={user} />}
+        <div>
         <Tab active={match.params.tab} user={match.params.user} />
+        </div>
       </div>
     </>
   );
